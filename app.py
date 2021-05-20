49b58f2ebd4aa6d448643e8883934ab7d3c1fdf2
Liu Jiawei <88jliu88@gmail.com>
2021-05-20 02:53:26 -0400

    feat: add temporary 404 error page

---
+++ app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from fastapi.exception_handlers import http_exception_handler
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+
+templates = Jinja2Templates(directory="templates")
+
+
+def render_template(path: str, request: Request, **kwargs):
+    return templates.TemplateResponse(path, {"request": request, **kwargs})
+
+
+@app.exception_handler(StarletteHTTPException)
+async def page_not_found_redirect(request: Request, exc: StarletteHTTPException):
+    if exc.status_code == 404:
+        return render_template("404.html", request)
+    return await http_exception_handler(request, exc)
